dc4cacb57be80a28a135f3452032681a1ab8c281
edsheeph <apilanedrian@gmail.com>
2022-04-01 13:55:51 +0800

    Code Fix

--- app/Classes/EmploymentDataClass.php
+++ app/Classes/EmploymentDataClass.php
@@ -25,7 +25,7 @@ class EmploymentDataClass
         $employmentData->usual_occupation_id = $request->usual_occupation_id;
         $employmentData->class_worker_id = $request->class_worker_id;
         $employmentData->work_affiliation_id = $request->work_affiliation_id;
-        $employmentData->place_work_type = $request->place_work_type;
+        $employmentData->place_work_type = !empty($request->place_work_type) ? $request->place_work_type : 0;
         $employmentData->place_work_type_specify = !empty($request->place_work_type_specify) ? $request->place_work_type_specify : "";
         $employmentData->employment = !empty($request->employment) ? $request->employment : "";
         $employmentData->employment_address = !empty($request->employment_address) ? $request->employment_address : "";
