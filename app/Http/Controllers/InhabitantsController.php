48e6a5990c72d32e099cceebbe8e05413cf1bcc4
edsheeph <apilanedrian@gmail.com>
2022-04-01 11:25:32 +0800

    Code Fix

--- app/Http/Controllers/InhabitantsController.php
+++ app/Http/Controllers/InhabitantsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Throwable;
 use Helpers;
 use Session;
 use Validator;
@@ -17,116 +18,132 @@ use App\Models\User;
 class InhabitantsController extends Controller
 {
     public function getInhabitantsList(Request $request) {
-        $peronalDataList = PersonalData::select(
-            'personal_data.id',
-            'personal_data.user_id',
-            'personal_data.application_id',
-            'personal_data.first_name',
-            'personal_data.middle_name',
-            'personal_data.last_name',
-            'users.barangay_id',
-            'barangays.description as barangay_desc',
-            'personal_data.birth_date',
-            'users.user_type_id',
-            'residence_application.status_id',
-            'residence_status.description as status_desc',
-            'personal_data.created_at as date_requested'
-        )
-        ->join("users", "users.id", "personal_data.user_id")
-        ->join("barangays", "barangays.id", "users.barangay_id")
-        ->join("residence_application", "residence_application.user_id", "users.id")
-        ->join("residence_status", "residence_status.id", "residence_application.status_id");
-
-        if ($request->search) {
-            $peronalDataList = $peronalDataList->where(function($q) use($request){
-                $q->orWhereRaw("personal_data.application_id LIKE ?","%".$request->search."%");
-                $q->orWhereRaw("CONCAT_WS(' ',CONCAT(personal_data.last_name,','),personal_data.first_name,personal_data.first_name) LIKE ?","%".$request->search."%");
-            });
+        try {
+            $peronalDataList = PersonalData::select(
+                'personal_data.id',
+                'personal_data.user_id',
+                'personal_data.application_id',
+                'personal_data.first_name',
+                'personal_data.middle_name',
+                'personal_data.last_name',
+                'users.barangay_id',
+                'barangays.description as barangay_desc',
+                'personal_data.birth_date',
+                'users.user_type_id',
+                'residence_application.status_id',
+                'residence_status.description as status_desc',
+                'personal_data.created_at as date_requested'
+            )
+            ->join("users", "users.id", "personal_data.user_id")
+            ->join("barangays", "barangays.id", "users.barangay_id")
+            ->join("residence_application", "residence_application.user_id", "users.id")
+            ->join("residence_status", "residence_status.id", "residence_application.status_id");
+    
+            if ($request->search) {
+                $peronalDataList = $peronalDataList->where(function($q) use($request){
+                    $q->orWhereRaw("personal_data.application_id LIKE ?","%".$request->search."%");
+                    $q->orWhereRaw("CONCAT_WS(' ',CONCAT(personal_data.last_name,','),personal_data.first_name,personal_data.first_name) LIKE ?","%".$request->search."%");
+                });
+            }
+    
+            if ($request->barangay_id) {
+                $peronalDataList = $peronalDataList->where("users.barangay_id", $request->barangay_id);
+            }
+    
+            if (!empty($request->sort) && !empty($request->order)) {
+                $peronalDataList = $peronalDataList->orderBy("personal_data.".$request->sort, strtoupper($request->order));
+            } else {
+                $peronalDataList = $peronalDataList->orderBy("residence_application.status_id", "DESC");
+                $peronalDataList = $peronalDataList->orderBy("personal_data.id", "DESC");
+            }
+    
+            $peronalDataList = $peronalDataList->paginate(
+                (int) $request->get('per_page', 10),
+                ['*'],
+                'page',
+                (int) $request->get('page', 1)
+            );
+    
+            return customResponse()
+                ->message("List of applicants.")
+                ->data($peronalDataList)
+                ->success()
+                ->generate();
+        } catch (\Throwable $th) {
+            return customResponse()
+                ->message("Sorry, Something went wrong.")
+                ->data(null)
+                ->success()
+                ->generate();
         }
-
-        if ($request->barangay_id) {
-            $peronalDataList = $peronalDataList->where("users.barangay_id", $request->barangay_id);
-        }
-
-        if (!empty($request->sort) && !empty($request->order)) {
-            $peronalDataList = $peronalDataList->orderBy("personal_data.".$request->sort, strtoupper($request->order));
-        } else {
-            $peronalDataList = $peronalDataList->orderBy("residence_application.status_id", "DESC");
-            $peronalDataList = $peronalDataList->orderBy("personal_data.id", "DESC");
-        }
-
-        $peronalDataList = $peronalDataList->paginate(
-            (int) $request->get('per_page', 10),
-            ['*'],
-            'page',
-            (int) $request->get('page', 1)
-        );
-
-        return customResponse()
-            ->message("List of applicants.")
-            ->data($peronalDataList)
-            ->success()
-            ->generate();
     }
 
     public function getResidenceList(Request $request) {
-        $resident = 5;
-        $peronalDataList = PersonalData::select(
-            'personal_data.id',
-            'personal_data.user_id',
-            'personal_data.application_id',
-            'personal_data.resident_id',
-            'personal_data.first_name',
-            'personal_data.middle_name',
-            'personal_data.last_name',
-            'personal_data.emergency_contact_no',
-            'users.barangay_id',
-            'barangays.description as barangay_desc',
-            'personal_data.birth_date',
-            'users.user_type_id',
-            'residence_application.status_id',
-            'residence_status.description as status_desc',
-            'personal_data.created_at as date_requested',
-            'barangay_id_generated.date_printed',
-            'barangay_id_generated.status'
-        )
-        ->join("users", "users.id", "personal_data.user_id")
-        ->join("barangays", "barangays.id", "users.barangay_id")
-        ->join("residence_application", "residence_application.user_id", "users.id")
-        ->join("residence_status", "residence_status.id", "residence_application.status_id")
-        ->leftJoin("barangay_id_generated", "barangay_id_generated.user_id", "users.id")
-        ->where("users.user_type_id", $resident);
+        try {
+            $resident = 5;
+            $peronalDataList = PersonalData::select(
+                'personal_data.id',
+                'personal_data.user_id',
+                'personal_data.application_id',
+                'personal_data.resident_id',
+                'personal_data.first_name',
+                'personal_data.middle_name',
+                'personal_data.last_name',
+                'personal_data.emergency_contact_no',
+                'users.barangay_id',
+                'barangays.description as barangay_desc',
+                'personal_data.birth_date',
+                'users.user_type_id',
+                'residence_application.status_id',
+                'residence_status.description as status_desc',
+                'personal_data.created_at as date_requested',
+                'barangay_id_generated.date_printed',
+                'barangay_id_generated.status'
+            )
+            ->join("users", "users.id", "personal_data.user_id")
+            ->join("barangays", "barangays.id", "users.barangay_id")
+            ->join("residence_application", "residence_application.user_id", "users.id")
+            ->join("residence_status", "residence_status.id", "residence_application.status_id")
+            ->leftJoin("barangay_id_generated", "barangay_id_generated.user_id", "users.id")
+            ->where("users.user_type_id", $resident);
 
-        if ($request->search) {
-            $peronalDataList = $peronalDataList->where(function($q) use($request){
-                $q->orWhereRaw("personal_data.application_id LIKE ?","%".$request->search."%");
-                $q->orWhereRaw("CONCAT_WS(' ',CONCAT(personal_data.last_name,','),personal_data.first_name,personal_data.first_name) LIKE ?","%".$request->search."%");
-            });
+            if ($request->search) {
+                $peronalDataList = $peronalDataList->where(function($q) use($request){
+                    $q->orWhereRaw("personal_data.application_id LIKE ?","%".$request->search."%");
+                    $q->orWhereRaw("CONCAT_WS(' ',CONCAT(personal_data.last_name,','),personal_data.first_name,personal_data.first_name) LIKE ?","%".$request->search."%");
+                });
+            }
+
+            if ($request->barangay_id) {
+                $peronalDataList = $peronalDataList->where("users.barangay_id", $request->barangay_id);
+            }
+
+            if (!empty($request->sort) && !empty($request->order)) {
+                $peronalDataList = $peronalDataList->orderBy("personal_data.".$request->sort, strtoupper($request->order));
+            } else {
+                $peronalDataList = $peronalDataList->orderBy("residence_application.status_id", "DESC");
+                $peronalDataList = $peronalDataList->orderBy("personal_data.id", "DESC");
+            }
+
+            $peronalDataList = $peronalDataList->paginate(
+                (int) $request->get('per_page', 10),
+                ['*'],
+                'page',
+                (int) $request->get('page', 1)
+            );
+
+            return customResponse()
+                ->message("List of residence.")
+                ->data($peronalDataList)
+                ->success()
+                ->generate();
+        } catch (\Throwable $th) {
+            return customResponse()
+                ->message("Sorry, Something went wrong.")
+                ->data(null)
+                ->success()
+                ->generate();
         }
-
-        if ($request->barangay_id) {
-            $peronalDataList = $peronalDataList->where("users.barangay_id", $request->barangay_id);
-        }
-
-        if (!empty($request->sort) && !empty($request->order)) {
-            $peronalDataList = $peronalDataList->orderBy("personal_data.".$request->sort, strtoupper($request->order));
-        } else {
-            $peronalDataList = $peronalDataList->orderBy("residence_application.status_id", "DESC");
-            $peronalDataList = $peronalDataList->orderBy("personal_data.id", "DESC");
-        }
-
-        $peronalDataList = $peronalDataList->paginate(
-            (int) $request->get('per_page', 10),
-            ['*'],
-            'page',
-            (int) $request->get('page', 1)
-        );
-
-        return customResponse()
-            ->message("List of residence.")
-            ->data($peronalDataList)
-            ->success()
-            ->generate();
     }
 
     public function show(Request $request, $id) {
